1ece4acc1513ed0d8443529f7c3e44467167bf2e
djkaz <justbekazu@gmail.com>
2021-04-02 16:59:15 -0500

    part one done

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,26 @@
-var getUserRepos = function(){
-    fetch("https://api.github.com/users/octocat/repos");
-    console.log("function was called");
+var getUserRepos = function(user) {
+
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+    
+
+    // fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+    //    console.log("inside", response); 
+    // });
+    // console.log(response);
 };
 getUserRepos();
+
+var response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+    response.json().then(function(data) {
+        console.log(data);
+    });
+});
+console.log(response);
